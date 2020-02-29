bc254ce58535c623ef3ec66a6aad5d23543b1d47
Louis Racicot <info@louisracicot.com>
2020-02-29 17:20:01 -0500

    gst

--- test/unit/handlers/post/get-all-posts.test.js
+++ test/unit/handlers/post/get-all-posts.test.js
@@ -1,24 +1,27 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-expressions */
 const AWS = require('aws-sdk-mock');
+const sinon = require('sinon');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
 
+chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
 const { expect } = chai;
 
 const lambda = require('../../../../src/handlers/post/get-all-posts');
 const getAllPostsEvent = require('../../../events/post/event-get-all-posts.json');
-// const getAllPostsEventLimit = require('../../../events/post/event-get-all-posts-limit.json');
-// const getAllPostsEventLimitOffset = require(
-// '../../../events/post/event-get-all-posts-limit-offset.json'
-// );
+const getAllPostsEventLimit = require('../../../events/post/event-get-all-posts-limit.json');
 const fakePosts = require('../../../data/posts');
 
 describe('Test getAllPosts handler', () => {
+  let scanTableSpy;
+
   before(() => {
-    AWS.mock('DynamoDB.DocumentClient', 'scan', async () => ({ Items: fakePosts }));
+    scanTableSpy = sinon.stub().returns({ Items: fakePosts });
+    AWS.mock('DynamoDB.DocumentClient', 'scan', async data => scanTableSpy(data));
   });
 
   after(() => {
@@ -37,29 +40,12 @@ describe('Test getAllPosts handler', () => {
     expect(result.body).to.eql(expectedResult.body);
   });
 
-  // it('should return a limited number of post', async () => {
-  //   const result = await lambda(getAllPostsEventLimit);
-  //
-  //   const expectedResult = {
-  //     statusCode: 200,
-  //     body: JSON.stringify([fakePosts[0], fakePosts[1]]),
-  //   };
-  //
-  //   expect(result.statusCode).to.eql(expectedResult.statusCode);
-  //   expect(JSON.parse(result.body).length).to.eql(2);
-  //   expect(result.body).to.eql(expectedResult.body);
-  // });
+  it('should return a limited number of post', async () => {
+    const result = await lambda(getAllPostsEventLimit);
 
-  // it('should return a limited number of post with offset', async () => {
-  //   const result = await lambda(getAllPostsEventLimitOffset);
-  //
-  //   const expectedResult = {
-  //     statusCode: 200,
-  //     body: JSON.stringify([fakePosts[1], fakePosts[2]]),
-  //   };
-  //
-  //   expect(result.statusCode).to.eql(expectedResult.statusCode);
-  //   expect(JSON.parse(result.body).length).to.eql(2);
-  //   expect(result.body).to.eql(expectedResult.body);
-  // });
+    expect(result.statusCode).to.eql(200);
+    expect(scanTableSpy).to.have.been.calledWithMatch({
+      Limit: '2',
+    });
+  });
 });
